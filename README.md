66453ce972544facd02ef029f1e78860e742c2c6
Dzemsad Dugalic <dzemsad.dugalic.fe@gmail.com>
2024-11-06 00:48:03 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,65 @@
+# AASHE Auth0 Auth
+
+Allows AASHE applications to authenticate through Auth0.
+
+This project uses `mozilla-django-oidc` under the hood.
+
+## Installation
+
+Install the package:
+
+```
+pip install git+https://github.com/aashe/aashe-auth0-auth.git@master
+```
+
+Add the Auth0Backend to your AUTHENTICATION_BACKENDS setting:
+
+```
+AUTHENTICATION_BACKENDS = (
+    ...
+    'auth0_auth.backends.Auth0Backend',
+)
+```
+
+Edit your urls.py to include:
+
+```
+urlpatterns = [
+    url(r'^auth0/', include('auth0_auth.urls')),
+    ...
+]
+```
+
+## Settings
+
+Add the required env vars and settings
+
+```
+BASE_URL = os.environ.get("BASE_URL", "http://127.0.0.1:8080")
+AUTH0_DOMAIN = os.environ.get("AUTH0_DOMAIN", )
+OIDC_RP_CLIENT_ID = os.environ.get("AUTH0_CLIENT_ID")
+OIDC_RP_CLIENT_SECRET = os.environ.get("AUTH0_CLIENT_SECRET")
+OIDC_RP_SCOPES = os.environ.get("OIDC_RP_SCOPES", "openid profile email")
+
+CUSTOM_OIDC_GROUPS_CLAIM = "https://accounts-aashe.aashe.org/roles"
+AUTH0_LOGOUT_ENDPOINT = f"https://{AUTH0_DOMAIN}/v2/logout"
+ALLOW_LOGOUT_GET_METHOD = True
+LOGIN_REDIRECT_URL = f"{BASE_URL}/django-admin/"
+LOGOUT_REDIRECT_URL = f"{BASE_URL}/django-admin/logout/"
+OIDC_OP_JWKS_ENDPOINT = f"https://{AUTH0_DOMAIN}/.well-known/jwks.json"
+OIDC_RP_SIGN_ALGO = "RS256"
+OIDC_OP_LOGOUT_URL_METHOD = "aashe_accounts.support.oidc_helpers.provider_logout"
+# So we can configure it dynamically
+try:
+    document = requests.get(f"https://{AUTH0_DOMAIN}/.well-known/openid-configuration").json()
+    OIDC_OP_AUTHORIZATION_ENDPOINT = document["authorization_endpoint"]
+    OIDC_OP_TOKEN_ENDPOINT = document["token_endpoint"]
+    OIDC_OP_USER_ENDPOINT = document["userinfo_endpoint"]
+except requests.exceptions.ConnectionError:
+    print("Skipping configuration for OIDC! It won't work correctly")
+    OIDC_OP_AUTHORIZATION_ENDPOINT = None
+    OIDC_OP_TOKEN_ENDPOINT = None
+    OIDC_OP_USER_ENDPOINT = None
+OIDC_VERIFY_SSL = True
+
+```
---
+++ auth0_auth/backends.py
@@ -0,0 +1,99 @@
+from django.contrib.auth.models import Group
+from django.db.models import QuerySet
+from mozilla_django_oidc.auth import OIDCAuthenticationBackend
+
+from aashe_accounts.settings import base as settings
+from aashe_accounts.support.http_utils import build_url_with_query_strings
+
+
+def provider_logout(request):
+    params = {
+        "returnTo": settings.LOGOUT_REDIRECT_URL,
+        "client_id": settings.OIDC_RP_CLIENT_ID,
+    }
+    return build_url_with_query_strings(settings.AUTH0_LOGOUT_ENDPOINT, params)
+
+
+class CustomOIDCAuthenticationBackend(OIDCAuthenticationBackend):
+    def verify_claims(self, claims) -> bool:
+        groups = self._retrieve_groups_from_claims(claims)
+        return OIDCToDjangoGroupsMapping.has_any_valid_group(groups)
+
+    def create_user(self, claims):
+        # Basic attributes
+        
+        user_id = claims["sub"].replace("|", "_")
+        email = claims.get("email")
+        given_name = claims.get("given_name", "")
+        family_name = claims.get("family_name", "")
+        # User creation
+        created_user = self.UserModel.objects.create_user(
+            user_id,
+            email=email,
+            first_name=given_name,
+            last_name=family_name,
+            is_staff=True,
+        )
+        # Relationships
+        self._fill_user_with_groups(created_user, claims)
+        created_user.save()
+        return created_user
+
+    def update_user(self, user, claims):
+        # Basic attributes
+        given_name = claims.get("given_name", "")
+        family_name = claims.get("family_name", "")
+        # User update
+        user.first_name = given_name
+        user.last_name = family_name
+        # Relationships
+        self._fill_user_with_groups(user, claims)
+        user.save()
+        return user
+
+    def filter_users_by_claims(self, claims):
+        qs_users = super().filter_users_by_claims(claims)
+        if not qs_users:
+            user_id = claims["sub"].replace("|", "_")
+            return self.UserModel.objects.filter(username__iexact=user_id)
+        return qs_users
+
+    @staticmethod
+    def _retrieve_groups_from_claims(claims: dict):
+        return claims.get(settings.CUSTOM_OIDC_GROUPS_CLAIM, [])
+
+    @classmethod
+    def _fill_user_with_groups(cls, user, claims):
+        groups = cls._retrieve_groups_from_claims(claims)
+        django_groups = OIDCToDjangoGroupsMapping.retrieve_django_groups(groups)
+        user.groups.set(django_groups)
+
+
+class OIDCToDjangoGroupsMapping:
+    oidc_groups_with_django_groups = [
+        ("AASHE_ACCOUNT_EMAIL_ONLY", "email"),
+        ("AASHE_ACCOUNT_BASIC", "basic"),
+        ("AASHE_ACCOUNT_TEMPORARY_MEMBER", "temporary_member"),
+        ("AASHE_ACCOUNT_MEMBER", "member"),
+        ("AASHE_ACCOUNT_STAFF", "staff"),
+    ]
+
+    @classmethod
+    def retrieve_django_groups(cls, groups: list[str]) -> QuerySet:
+        selected_groups = cls._retrieve_django_groups(groups)
+        return Group.objects.filter(name__in=selected_groups)
+
+    @classmethod
+    def has_any_valid_group(cls, groups: list[str]) -> bool:
+        return any(cls._retrieve_django_groups(groups))
+
+    @classmethod
+    def _retrieve_django_groups(cls, groups: list[str]):
+        django_groups = []
+
+        for oidc_group, django_group in cls.oidc_groups_with_django_groups:
+            is_group_configured = oidc_group in groups
+            if is_group_configured:
+                django_groups.append(django_group)
+
+        return django_groups

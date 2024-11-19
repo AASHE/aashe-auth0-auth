1a60ad4650456b0fb463a4bb3a97e9eb9dd1122d
Dzemsad Dugalic <dzemsad.dugalic.fe@gmail.com>
2024-11-19 13:54:02 +0100

    fix: warning about def value

--- auth0_auth/models.py
+++ auth0_auth/models.py
@@ -5,7 +5,7 @@ class Organization(models.Model):
     id = models.CharField(_("Auth0 Organization ID"), max_length=255, primary_key=True)
     name = models.CharField(max_length=50)
     display_name = models.CharField(max_length=255)
-    metadata = models.JSONField(default={})
+    metadata = models.JSONField(default=dict)
 
     class Meta:
         verbose_name = _("Organization")

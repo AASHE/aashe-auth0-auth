27bbcdb14abcd2401a9453589b4dbbd8f286f639
Dzemsad Dugalic <dzemsad.dugalic.fe@gmail.com>
2024-11-19 13:40:45 +0100

    fix: typo

--- auth0_auth/models.py
+++ auth0_auth/models.py
@@ -1,7 +1,8 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 class Organization(models.Model):
-    id = models.models.CharField(_("Auth0 Organization ID"), max_length=255, primary_key=True)
+    id = models.CharField(_("Auth0 Organization ID"), max_length=255, primary_key=True)
     name = models.CharField(max_length=50)
     display_name = models.CharField(max_length=255)
     metadata = models.JSONField(default={})
